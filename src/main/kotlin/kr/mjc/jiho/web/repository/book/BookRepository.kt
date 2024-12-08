a7c62db861878c90533e9fcfce1084331a6d66d6
JayJeong <80833894+JayJeong1741@users.noreply.github.com>
2024-12-08 17:03:47 +0900

    update title

--- src/main/kotlin/kr/mjc/jiho/web/repository/book/BookRepository.kt
+++
@@ -1,9 +0,0 @@
-package kr.mjc.jiho.web.repository.book
-
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Slice
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface BookRepository : JpaRepository<Book, Long> {
-    fun findAllByOrderByIdDesc(pageable: PageRequest): Slice<Book>
-}
